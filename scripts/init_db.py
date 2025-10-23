05bf294a607c4966a2ddfd3551f15ac7e8425e88
shengshanxiang <ssxmojc@163.com>
2025-10-23 18:09:46 +0800

    fix model

--- scripts/init_db.py
+++
@@ -1,8 +0,0 @@
-
-from model.af_data import AfAppDataDAO
-from model.cookie import CookieDAO
-from model.user_app_data import UserAppDataDAO
-
-UserAppDataDAO.init_table()
-AfAppDataDAO.init_table()
-CookieDAO._init_table()
--- scripts/system_init.py
+++ scripts/system_init.py
@@ -1,2 +1,8 @@
 
-import scripts.init_db
+from model.af_data import AfAppDataDAO
+from model.cookie import CookieDAO
+from model.user_app_data import UserAppDataDAO
+
+UserAppDataDAO.init_table()
+AfAppDataDAO.init_table()
+CookieDAO._init_table()
